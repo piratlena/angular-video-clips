f04a23b5d6a2b581d533d000b2c1553d9506fb4b
Elena Brazhnikova <lenovosibirsk@mail.ru>
2023-10-22 10:35:08 +0700

    add an uploading form

--- src/app/pages/upload/upload.component.ts
+++ src/app/pages/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-upload',
@@ -7,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
   isDragOver = false;
+  file: File | null = null;
+  nextStep = false;
+
+  title = new FormControl('', {
+    validators: [Validators.required, Validators.minLength(3)],
+    nonNullable: true,
+  });
+  uploadForm = new FormGroup({
+    title: this.title,
+  });
 
   ngOnInit(): void {}
 
   storeFile($event: Event) {
     this.isDragOver = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
+    this.nextStep = true;
+  }
+  uploadFile() {
+    console.log('File uploaded');
   }
 }
--- src/app/video/video.module.ts
+++ src/app/video/video.module.ts
@@ -4,9 +4,15 @@ import { ManageComponent } from '../pages/manage/manage.component';
 import { UploadComponent } from '../pages/upload/upload.component';
 import { VideoRoutingModule } from './video-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ManageComponent, UploadComponent],
-  imports: [CommonModule, VideoRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    VideoRoutingModule,
+    SharedModule,
+    ReactiveFormsModule,
+  ],
 })
 export class VideoModule {}
